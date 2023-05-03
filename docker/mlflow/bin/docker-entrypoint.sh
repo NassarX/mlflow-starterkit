708f02681f5d93c3d6872b67908e858407ea2fdd
NassarX <mahmoud@nassarx.com>
2023-05-04 00:50:07 +0100

    Fix permissions issue preventing MLflow from saving models
    Previously, MLflow was unable to save models due to a permission denied error. The issue was caused by the lack of proper permissions for the user running the MLflow container. To fix this, I added a `jovyan` user with the correct permissions and made sure that the user had access to the necessary directories. This allowed me to successfully save models from the MLflow UI and notebook.
    
    This commit fixes the permission issue by updating the Dockerfile and creating a `jovyan` user with the necessary permissions.

---
+++ docker/mlflow/bin/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$MLFLOW_BACKEND_STORE" ]; then
+  echo >&2 "MLFLOW DIR must be set"
+  exit 1
+fi
+
+mlflow server \
+    --backend-store-uri "$MLFLOW_TRACKING_URI" \
+    --default-artifact-root $MLFLOW_TRACKING_URI \
+    --host 0.0.0.0 \
+    --port $MLFLOW_SERVER_PORT
