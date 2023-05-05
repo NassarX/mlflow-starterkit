10cb36bcca1e38bd430193ae4835b5a29a1bb2a0
NassarX <mahmoud@nassarx.com>
2023-05-05 04:20:07 +0100

    Update startup code to check if model path exists

--- app/main.py
+++ app/main.py
@@ -12,19 +12,23 @@ app = FastAPI()
 
 @app.get("/")
 def check():
-    model = mlflow.pyfunc.load_model("models/3/809080238ef74723b258f2af51dc81ee/artifacts/vader_model")
-    queries = ["This is a bad class. I hate MLOps and the professor! :-C. But our campus is good.",
-               "Lovely weather during the weekend.",
-               "LOL, this guy fell off a chair while listening the professor.",
-               "This is INSANE! How can you do such TERRIBLE thing?????"]
+    model_path = os.path.join(os.getcwd(), "models/3/809080238ef74723b258f2af51dc81ee/artifacts/vader_model")
+    if os.path.exists(model_path):
+        model = mlflow.pyfunc.load_model("models/3/809080238ef74723b258f2af51dc81ee/artifacts/vader_model")
+        queries = ["This is a bad class. I hate MLOps and the professor! :-C. But our campus is good.",
+                   "Lovely weather during the weekend.",
+                   "LOL, this guy fell off a chair while listening the professor.",
+                   "This is INSANE! How can you do such TERRIBLE thing?????"]
 
-    scores_ = []
-    for q in queries:
-        m_input = pd.DataFrame([q])
-        scores = model.predict(m_input)
-        scores_.append([q, str(scores[0])])
+        scores_ = []
+        for q in queries:
+            m_input = pd.DataFrame([q])
+            scores = model.predict(m_input)
+            scores_.append([q, str(scores[0])])
 
-    return {"scores": scores_}
+            return {"scores": scores_}
+    else:
+        return {"error": "Model not found!"}
 
 
 if __name__ == "__main__":
